4f1086206411528d33a0353821ec8ba61b155e04
Dong Le <donglp1810@gmail.com>
2025-09-23 10:26:38 +0700

    Normalize all line endings to LF

--- app/src/main/java/com/dong/reader/MainActivity.kt
+++ app/src/main/java/com/dong/reader/MainActivity.kt
@@ -1,17 +1,18 @@
 package com.dong.reader
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.dong.reader.ui.theme.DongReaderTheme
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +21,24 @@ class MainActivity : ComponentActivity() {
         setContent {
             DongReaderTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Dong",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    val db = FirebaseFirestore.getInstance()
+                    val user: MutableMap<String, Any> = HashMap()
+                    user["firstName"] = "Dong"
+                    user["lastName"] = "Le"
+                    db.collection("users")
+                        .add(user)
+                        .addOnSuccessListener {
+                            Log.d("TAG", "DocumentSnapshot added with ID: " + it.id)
+                        }
+                        .addOnFailureListener {
+                            Log.w("TAG", "Error adding document", it)
+                        }
+
+                    Surface(modifier = Modifier.padding(innerPadding)) {
+                        Text(text = "Hello World")
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DongReaderTheme {
-        Greeting("Android")
     }
 }
